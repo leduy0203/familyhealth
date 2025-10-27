de5f309bb320a6695b17757b1f3d34a97764132c
leduy0203 <lecongduy0203@gmail.com>
2025-10-27 21:59:14 +0700

    first commit

---
+++ src/test/java/vn/familyhealth/FamilyHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.familyhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
